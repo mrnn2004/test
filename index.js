f03dd98273e5af1ecab2cc4ea9ec52de77db1505
Ali <50074052+alirezaarzehgar@users.noreply.github.com>
2022-07-02 21:45:39 +0430

    Refactoring: better coding style
    change import orders.
    Change identity.
    Remove white-spaces.

--- index.js
+++ index.js
@@ -1,31 +1,19 @@
-const path=require("path");
-
-const http=require("http");
-
-const express=require('express');
-
-const socketio=require('socket.io');
-
-const app=express();
-
-const server=http.createServer(app);
-
-const io=socketio(server);
-
-const port=process.env.PORT || 3000;
-
-const publicDirectoryPath=path.join(__dirname,"/public");
+const app = express();
+const path = require("path");
+const http = require("http");
+const express = require('express');
+const socketio = require('socket.io');
+const server = http.createServer(app);
+const io = socketio(server);
+const port = process.env.PORT || 3000;
+const publicDirectoryPath = path.join(__dirname,"/public");
 
 app.use(express.static(publicDirectoryPath));
 
 io.on("connection",(client)=>{
-
-console.log('New websocket connection');
-
+  console.log('New websocket connection');
 })
 
 server.listen(port,()=>{
-
-console.log(`Server is up on port ${port}!`);
-
+  console.log(`Server is up on port ${port}!`);
 })
